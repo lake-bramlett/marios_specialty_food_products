e2d24cfbc6a3e8174c71c4b00c280a2ec8db698d
Lake Bramlett <lakebramlett@Lakes-MBP.hsd1.or.comcast.net>
2019-08-09 12:41:40 -0700

    adds review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :product
-  validates :author, presence: true
-  validates :content_body, presence: true
+  validates :author, :content_body, :rating, presence: true
+  validates_length_of :content_body, minimum: 50, maximum: 250
+  validates :rating, :inclusion => 1..5
 end
