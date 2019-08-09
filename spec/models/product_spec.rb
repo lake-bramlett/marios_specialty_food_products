826a6c978080f879b28502df1af40f393e71525c
Lake Bramlett <lakebramlett@Lakes-MBP.hsd1.or.comcast.net>
2019-08-09 13:04:11 -0700

    add titleize callback and spec test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,4 +4,8 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
+  it("titleizes the name of a product") do
+    product = Product.create({name: "big hat", cost: 12, country_of_origin: "United States"})
+    expect(product.name).to(eq("Big Hat"))
+  end
 end
