dc0546be9a53aa313d5df21fe4fddc833652df1d
Lake Bramlett <lakebramlett@Lakes-MBP.hsd1.or.comcast.net>
2019-08-09 13:57:59 -0700

    refine seeding parameters

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,11 @@
 Product.destroy_all
 
 50.times do |index|
-  Product.create!(name: Faker::Commerce.product_name,
-                        cost: Faker::Number.decimal(l_digits: 2),
-                        country_of_origin: Faker::Address.country)
-                        # rand(2..4).times do
-                        #   author = Faker::Name.name
-                        #   content_body = Faker::Lorem.paragraph(sentence_count: rand(1..5))
-                        #   rating = Faker::Number.between(from: 1, to: 10)
-                        #   review = Review.new(author: author, content_body: content_body, rating: rating)
-                        #   review.save!
-                        # end
+  product = Product.create!(name: Faker::Food.dish, cost: Faker::Number.decimal(l_digits: 2), country_of_origin: Faker::Address.country)
+  rand(1..5).times do
+    product.reviews.new(author: Faker::Name.name, content_body: Faker::Lorem.characters(number: rand(50..250)), rating: rand(1..5))
+    product.save
+  end
 end
 
-p "Created #{Product.count} products"
+p "Created #{Product.count} products and #{Review.count} reviews"
