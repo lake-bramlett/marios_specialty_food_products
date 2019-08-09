95f8170f506bff287f2a7a7a7dd7c4efd5ac86b6
Lake Bramlett <lakebramlett@Lakes-MBP.hsd1.or.comcast.net>
2019-08-09 08:16:04 -0700

    add initial migrations

---
+++ db/migrate/20190809150927_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :cost
+      t.string :country_of_origin
+      
+      t.timestamps
+    end
+  end
+end
