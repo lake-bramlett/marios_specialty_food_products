0644d896c9acccc229bfc3e396d1b24c02d0a4bd
Lake Bramlett <lakebramlett@Lakes-MBP.hsd1.or.comcast.net>
2019-08-16 10:36:05 -0700

    add more spec tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,4 +9,20 @@ describe Product do
     product.save!
     expect(product.name).to(eq("Big Hat"))
   end
+  it("should create a product") do
+    product = Product.create({name: "big hat", cost: 12.21, country_of_origin: "United States"})
+    product.save!
+    expect(Product.all).to(eq([product]))
+  end
+  it("should edit a product") do
+    product = Product.create({name: "big hat", cost: 12.21, country_of_origin: "United States"})
+    product.save!
+    product.update!({name: "Bigger Hat"})
+    expect(product.name).to(eq("Bigger Hat"))
+  end
+  it("should delete a product") do
+    product = Product.create({name: "big hat", cost: 12.21, country_of_origin: "United States"})
+    product.destroy
+    expect(product).to(eq(nil))
+  end
 end
