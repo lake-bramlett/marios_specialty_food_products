22a83e8fe0f984785f80c85b5a0eca566c4672be
Lake Bramlett <lakebramlett@Lakes-MacBook-Pro.local>
2019-08-16 12:54:49 -0700

    upate products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -40,7 +40,7 @@ class ProductsController < ApplicationController
   def update
     @product= Product.find(params[:id])
       if @product.update(product_params)
-        redirect_to products_path
+        redirect_to product_path(params[:id])
       else
         render :edit
       end
