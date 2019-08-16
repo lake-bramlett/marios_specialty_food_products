f0b223b798e8514ca2ff97a57a073af343ef147f
Lake Bramlett <lakebramlett@Lakes-MBP.hsd1.or.comcast.net>
2019-08-16 10:16:30 -0700

    fix product_spec titleize unit test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,7 +5,8 @@ describe Product do
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
   it("titleizes the name of a product") do
-    product = Product.create({name: "big hat", cost: 12, country_of_origin: "United States"})
+    product = Product.create({name: "big hat", cost: 12.21, country_of_origin: "United States"})
+    product.save!
     expect(product.name).to(eq("Big Hat"))
   end
 end
