b64ccaafeb27a3bea5efb5383cec0b6e8aa0a7fa
Lake Bramlett <lakebramlett@Lakes-MacBook-Pro.local>
2019-08-16 11:36:21 -0700

    add integration test for deleting a review

--- spec/features/review_spec.rb
+++ spec/features/review_spec.rb
@@ -40,6 +40,25 @@ describe "the edit review process" do
     click_on 'commit'
     expect(page).to have_content 'BurgerHater42'
     expect(page).to have_content '1/5'
+  end
+end
 
+describe "the delete review process" do
+  it "delete a review to a product" do
+    visit root_path
+    click_link 'Add a product'
+    fill_in 'Name', :with => 'Cheeseburger'
+    fill_in 'Cost', :with => '12.21'
+    fill_in 'Country of origin', :with => 'United States'
+    click_on 'Create Product'
+    click_link 'Cheeseburger'
+    click_link 'Add a review'
+    fill_in 'Author', :with => 'Burgerlover42'
+    fill_in 'Content body', :with => 'These burgers are so great! Give me more of them burgers! Yum yum yum!'
+    fill_in 'Rating', :with => '5'
+    click_on 'commit'
+    click_link 'Edit review'
+    click_link 'Delete review'
+    expect(page).to_not have_content 'Burgerlover42'
   end
 end
