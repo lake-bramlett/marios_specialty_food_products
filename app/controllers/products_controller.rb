3fe85067be96b792a6184cfc934e2e1aecf1b5c5
Lake Bramlett <lakebramlett@Lakes-MBP.hsd1.or.comcast.net>
2019-08-16 10:09:00 -0700

    add initial intergration test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.create(product_params)
     if @product.save
+      flash[:notice] = "Product successfully added!"
       redirect_to products_path
     else
       render :new
--- spec/features/add_product_spec.rb
+++ spec/features/add_product_spec.rb
@@ -2,18 +2,17 @@ require 'rails_helper'
 
 describe "the add a product process" do
   it "adds a new album" do
-    visit root
+    visit root_path
     click_link 'Add a product'
-    fill_in 'Name', :with => 'Giant Steps'
-    fill_in 'Genre', :with => 'Jazz'
-    click_on 'Create Album'
-    expect(page).to have_content 'Album successfully added!'
-    expect(page).to have_content 'Giant Steps'
+    fill_in 'Name', :with => 'Cheeseburger'
+    fill_in 'Cost', :with => '12.21'
+    fill_in 'Country of origin', :with => 'United States'
+    click_on 'Create Product'
+    expect(page).to have_content 'Product successfully added!'
+    expect(page).to have_content 'Cheeseburger'
+    expect(page).to have_content '$12.21'
+    expect(page).to have_content 'United States'
   end
 
-  it "gives an error when no name is entered" do
-    visit new_album_path
-    click_on 'Create Album'
-    expect(page).to have_content "Name can't be blank"
-  end
+
 end
