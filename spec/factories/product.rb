32b37c5b59390b601d8d4e9655f214d311ec4282
Lake Bramlett <lakebramlett@Lakes-MBP.hsd1.or.comcast.net>
2019-08-16 17:39:10 -0700

    add review unit tests

--- spec/factories/product.rb
+++ spec/factories/product.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 FactoryBot.define do
    factory :product do
    name {"Big Hat"}
-   cost {12.00}
+   cost {12.01}
    country_of_origin {"USA"}
    end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -8,8 +8,16 @@ describe Review do
   it { should validate_length_of(:content_body).is_at_most(250) }
   it { should validate_inclusion_of(:rating).in_range(1..5) }
   it("should create a review") do
+    product = FactoryBot.create(:product)
     product.reviews.new(author: "Steve", content_body: "This is my jam. Love it. Never without it! Keep 'em coming!", rating: 5)
     product.save
-    expect(product.reviews(1).author).to(eq("Steve"))
+    expect(product.reviews[0].author).to(eq("Steve"))
+  end
+  it("should update a review") do
+    product = FactoryBot.create(:product)
+    product.reviews.new(author: "Steve", content_body: "This is my jam. Love it. Never without it! Keep 'em coming!", rating: 5)
+    product.save
+    product.reviews[0].update(author: "Glenn")
+    expect(product.reviews[0].author).to(eq("Glenn"))
   end
 end
